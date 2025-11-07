46287f3ef6082cef24bfb895a6ffe3bbed25fa6d
AyushmaanWorks <ayushmaan4096@gmail.com>
2025-11-08 05:24:53 +0530

    font fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { Press_Start_2P, Pixelify_Sans } from "next/font/google";
 
 const inter = Inter({
   variable: "--font-inter",
