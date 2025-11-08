bb54e3f9119230048dadbc9f7a0c5716789c066e
AyushmaanWorks <ayushmaan4096@gmail.com>
2025-11-08 05:38:38 +0530

    Add font configurations for Press Start 2P and Pixelify sans
    Added new font configurations for Press Start 2P and Pixelify Sans.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,25 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Press_Start_2P, Pixelify_Sans } from "next/font/google";
 
+
 const inter = Inter({
   variable: "--font-inter",
   weight: ["400", "700"],
   subsets: ["latin"],
+});
+
+const pressStart = Press_Start_2P({
+  subsets: ["latin"],
+  weight: "400",          // Press Start 2P only has 400
+  display: "swap",
+  variable: "--font-press-start",
+});
+
+const pixelify = Pixelify_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"], // choose what you need
+  display: "swap",
+  variable: "--font-pixelify",
 });
 
 export const metadata: Metadata = {
@@ -23,7 +38,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${pressStart.variable} ${pixelify.variable}`}>
       <body
         className={`antialiased mx-4 min-h-screen bg-black text-white p-4 ${inter.className} flex flex-col`}
       >
